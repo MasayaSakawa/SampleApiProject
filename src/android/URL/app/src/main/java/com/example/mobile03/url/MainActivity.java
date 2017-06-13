0e915ffcacfbd8a0977fdb652c5f237b4e2e965a
“Masaya <masaya.sakawa.sys@gmail.com>
2017-06-13 15:07:20 +0900

    feature #6 URL netに繋がらない

--- src/android/URL/app/src/main/java/com/example/mobile03/url/MainActivity.java
+++ src/android/URL/app/src/main/java/com/example/mobile03/url/MainActivity.java
@@ -11,55 +11,44 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    //URL
+    private EditText editText1 = null;
+    //商品名
+    private EditText editText2 = null;
+    //名前
+    private EditText editText3 = null;
+    //sql
+    private String sql = null;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button) findViewById(R.id.button1);
+        //URL
+        editText1 = (EditText) findViewById(R.id.editText1);
+        //商品名
+        editText2 = (EditText) findViewById(R.id.editText2);
+        //名前
+        editText3 = (EditText) findViewById(R.id.editText3);
 
-        button1.setOnClickListener(new OnClickListener() {
-
-
-
-
-            public void onClick(View v) {
-
-                //URL
-                EditText editText1 = (EditText) findViewById(R.id.editText1);
-                //商品名
-                EditText editText2 = (EditText) findViewById(R.id.editText2);
-                //名前
-                EditText editText3 = (EditText) findViewById(R.id.editText3);
-
-
-
-                // 入力された文字を取得
-
-                String str1 = editText1.getText().toString();
-                String str2 = editText2.getText().toString();
-                String str3 = editText3.getText().toString();
-                String str4 = str1 + str2 + str3;
-
-                //API
-                String api = "api呼ぶ";
-
-
-                // 取得した文字をTextViewにセット！
-
-
-                Uri uri = Uri.parse("?" + str4 + api);
-                Intent i = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(i);
-
-
-            }
-
-
-        });
-
+        final Button button1 = (Button) findViewById(R.id.button1);
     }
-
-
+    public void onClick(View v) {
+        // 入力された文字を取得
+        String str1 = editText1.getText().toString();
+        String str2 = editText2.getText().toString();
+        String str3 = editText3.getText().toString();
+        String str4 = "?" + str2 + str3;
+        //押したボタンでの分岐
+        switch(v.getId()){
+            case R.id.button1:
+                sql = "";
+                break;
+        }
+        // 取得した文字をTextViewにセット！
+        Uri uri = Uri.parse(str1 + str4 + sql);
+        Intent i = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(i);
+    }
 }
